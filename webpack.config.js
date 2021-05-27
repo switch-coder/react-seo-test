8b0d87a95a31d94366de0b418a0d1ea4b14170dd
switch-coder <sang_hk@naver.com>
2021-05-27 23:54:53 +0900

    prerender SPA plugin 추가

--- webpack.config.js
+++ webpack.config.js
@@ -26,5 +26,14 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins:[
+        new PrerenderSPAPlugin({
+            // Required - The path to the webpack-outputted app to prerender.
+            staticDir: path.join(__dirname, 'dist'),
+            // Required - Routes to render.
+            routes: [ '/', '/about', '/some/deep/nested/route' ],
+          })
+    ]
+    
 }
