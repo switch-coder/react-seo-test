646c8fa8fba26d244b4952cd2a4b0b1d3b704948
switch-coder <sang_hk@naver.com>
2021-05-28 16:52:04 +0900

    api 주소 변경

--- src/models/EventsModel.js
+++ src/models/EventsModel.js
@@ -2,7 +2,7 @@ import request from "./request";
 
 export default{
     async list() {
-        const data = await request("get","http://localhost:3001/api/getEvents");
+        const data = await request("get","http://ec2-54-180-118-162.ap-northeast-2.compute.amazonaws.com:3001/api/getEvents");
         console.log(data)
         return data;
     }
