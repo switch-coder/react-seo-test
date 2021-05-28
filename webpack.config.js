1d8d13eee9214dab2548bc5cb125d7afc69f2a96
switch-coder <sang_hk@naver.com>
2021-05-28 17:08:35 +0900

    prerender sqa plugin 설정 제외

--- webpack.config.js
+++ webpack.config.js
@@ -46,39 +46,39 @@ module.exports = {
         new webpack.BannerPlugin({
             banner : `빌드 날짜: ${new Date().toLocaleString()}`
         }),
-        new PrerenderSPAPlugin({ 
-            // routes: ["/" , "/detail/1","/detail/2","/detail/3"], 
-            // staticDir: path.join(__dirname, './public'),
-            // postProcess(renderedRoute) {
-            //     let { html , route} = renderedRoute;
-            //     const {name , meta , image} = seo.find(i => `/detail/${i.id}` === route)
-            //     const metaData = `<title>${name}</title>` +
-            //      `<meta name="title" content="${name}" />` + 
-            //      `<meta name="description" content="${meta.join(',')}" />` + 
-            //      `<meta name="keywords" content="${meta}" />` + 
-            //      `<meta property="og:type" content="website" />` + 
-            //      `<meta property="og:title" content="${name}" />` + 
-            //      `<meta property="og:description" content="${meta.join(',')}" />` + 
-            //      `<meta property="og:image" content="${image}" />`
-            //      const start = html.indexOf('<head>') + '<head>'.length; 
-            //      html = html.slice(0, start) + metaData + html.slice(start); 
-            //      renderedRoute.html = html; 
-            //      console.log(html)
-            //      return renderedRoute;
+        // new PrerenderSPAPlugin({ 
+        //     // routes: ["/" , "/detail/1","/detail/2","/detail/3"], 
+        //     // staticDir: path.join(__dirname, './public'),
+        //     // postProcess(renderedRoute) {
+        //     //     let { html , route} = renderedRoute;
+        //     //     const {name , meta , image} = seo.find(i => `/detail/${i.id}` === route)
+        //     //     const metaData = `<title>${name}</title>` +
+        //     //      `<meta name="title" content="${name}" />` + 
+        //     //      `<meta name="description" content="${meta.join(',')}" />` + 
+        //     //      `<meta name="keywords" content="${meta}" />` + 
+        //     //      `<meta property="og:type" content="website" />` + 
+        //     //      `<meta property="og:title" content="${name}" />` + 
+        //     //      `<meta property="og:description" content="${meta.join(',')}" />` + 
+        //     //      `<meta property="og:image" content="${image}" />`
+        //     //      const start = html.indexOf('<head>') + '<head>'.length; 
+        //     //      html = html.slice(0, start) + metaData + html.slice(start); 
+        //     //      renderedRoute.html = html; 
+        //     //      console.log(html)
+        //     //      return renderedRoute;
 
-            // }, 
-            // renderer: new PrerenderSPAPlugin.PuppeteerRenderer({ 
-            //     maxConcurrentRoutes: 2,
-            //     args: ["--no-sandbox", "--disable-setuid-sandbox"], 
-            //     headless: false }) })
-            "routes" : [],
-            "staticDir" :path.join(__dirname,"public"),
-            "postProcess"(renderedRoute){},
-            "renderer":new PrerenderSPAPlugin.PuppeteerRenderer({
-              "args": ["--no--sandbox","--disable-setuid-sandbox"],
-              "headless":false
-            })})
-         ,
+        //     // }, 
+        //     // renderer: new PrerenderSPAPlugin.PuppeteerRenderer({ 
+        //     //     maxConcurrentRoutes: 2,
+        //     //     args: ["--no-sandbox", "--disable-setuid-sandbox"], 
+        //     //     headless: false }) })
+        //     "routes" : [],
+        //     "staticDir" :path.join(__dirname,"public"),
+        //     "postProcess"(renderedRoute){},
+        //     "renderer":new PrerenderSPAPlugin.PuppeteerRenderer({
+        //       "args": ["--no--sandbox","--disable-setuid-sandbox"],
+        //       "headless":false
+        //     })})
+        //  ,
         new MiniCssExtractPlugin({
             filename: path.join(__dirname, '../../../../../[id].css'),
             chunkFilename: "[id].css"
