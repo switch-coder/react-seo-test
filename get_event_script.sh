cef3ecef4d41e12bb16cfc850682f704ec57bff2
switch-coder <sang_hk@naver.com>
2021-05-28 16:54:53 +0900

    데이터 json 으로 파싱하는 부분 제거

--- get_event_script.sh
+++ get_event_script.sh
@@ -1,3 +1,6 @@
 OUTPUT=$(curl --location --request GET 'http://localhost:3001/api/getEvents') 
 echo "hi"
 echo "$OUTPUT" > seo.json
+
+
+# OUTPUT=$(curl http://ec2-54-180-118-162.ap-northeast-2.compute.amazonaws.com:3001/api/getEvents) && echo $OUTPUT > seo.json && 
