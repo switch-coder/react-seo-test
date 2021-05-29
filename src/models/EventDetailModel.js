ae24ad902d9521b96e553714b71de1c331ff303d
switch-coder <sang_hk@naver.com>
2021-05-29 18:04:11 +0900

    eventDetail api 주소 변경

--- src/models/EventDetailModel.js
+++ src/models/EventDetailModel.js
@@ -2,7 +2,7 @@ import request from "./request";
 
 export default{
     async getEventDetail(id) {
-        const data = await request("get",`http://ec2-54-180-118-162.ap-northeast-2.compute.amazonaws.com/api/eventDetail?id=${id}`);
+        const data = await request("get",`http://ec2-54-180-118-162.ap-northeast-2.compute.amazonaws.com:3001/api/eventDetail?id=${id}`);
         console.log(data)
         return data;
     }
